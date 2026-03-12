4da61d7ad7eaff9d8dc9839303e2b42fbc92cd2b
joecuatropatas-png <joecuatropatas@gmail.com>
2026-03-12 12:45:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask, request, jsonify
 import subprocess
 import uuid
+import boto3
+import os
 
 app = Flask(__name__)
+
+R2_ACCOUNT_ID = os.environ.get("R2_ACCOUNT_ID")
+R2_ACCESS_KEY = os.environ.get("R2_ACCESS_KEY")
+R2_SECRET_KEY = os.environ.get("R2_SECRET_KEY")
+R2_BUCKET = os.environ.get("R2_BUCKET")
+
+s3 = boto3.client(
+    "s3",
+    endpoint_url=f"https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com",
+    aws_access_key_id=R2_ACCESS_KEY,
+    aws_secret_access_key=R2_SECRET_KEY
+)
 
 @app.route("/download")
 def download():
@@ -17,8 +31,14 @@ def download():
         url
     ])
 
+    s3.upload_file(filename, R2_BUCKET, filename)
+
+    public_url = f"https://{R2_BUCKET}.r2.dev/{filename}"
+
+    os.remove(filename)
+
     return jsonify({
-        "file": filename
+        "video_url": public_url
     })
 
 if __name__ == "__main__":
